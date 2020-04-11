b2077d3a989af96ceecbb0b1316531721b33da34
jazmine14 <jazmine14@github.com>
2020-04-11 16:14:15 +0530

    commit

--- lab2 COMP6521/src/Index.java
+++ lab2 COMP6521/src/Index.java
@@ -2,8 +2,20 @@ import java.util.BitSet;
 import java.util.HashMap;
 
 public class Index {
-	BitSet bitSet = new BitSet();
 	
 	private HashMap<BitMap.BIKey, BitSet> bitmaps;
+	public Index() {
+		bitmaps = new HashMap<BitMap.BIKey, BitSet>();
+	}
+	
+	public void set(BitMap.BIKey key) {
+		BitSet bitset = bitmaps.get(key);
+		
+		if(bitset == null) {
+			bitset = new BitSet();
+			bitmaps.put(key, bitset);
+		}
+     }
+	
 	
 }
